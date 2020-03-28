ec7cfec5bb638f9fe79ac7f2cfc4c4c19cd0d3a6
40150688 <jjohnston498@qub.ac.uk>
2020-03-28 17:15:41 +0000

    updated paths for ligra-partition

--- benchmarks/ligra-partition/pagerank_bench.sh
+++ benchmarks/ligra-partition/pagerank_bench.sh
@@ -3,7 +3,7 @@
 # module load compilers/gcc-4.9.0
 
 export LD_LIBRARY_PATH="../../cilk-swan/lib/"  # req. lib
-export CILK_NWORKERS=8                                             # no. cpu threads
+export CILK_NWORKERS=4                                             # no. cpu threads
 #export LD_PRELOAD="./bin/interposer_cilk.so"                       # req. for cilk
 
 # note: large graphs should take about ~15 minutes to run
@@ -12,45 +12,47 @@ export CILK_NWORKERS=8                                             # no. cpu thr
 PR="PageRankUpdate PageRankUpdate_Floats PageRankManSeg PageRankManSeg_f PageRankUpdate_F2D PageRankManSeg_hybrid"
 #PR="PageRankManSeg_dev"
 
+BASE_PATH="graphs"
+
 date
-make ${PR} -j 8
+make ${PR} -j 4
 
 
 # use -rounds 10 and -c (no. cores * 16) [trying 48*16 rather than 96*16 for partitioning]
 echo ""
 for alg in $PR
 	do
-		echo "start ${alg} twitter_xl"
-		date
-		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 /var/shared/projects/asap/graphs/adj/realworld/recent/twitter_undir_xl > pr_out/twitter_undir_xl_${alg}.txt 2>&1
-		date
-		echo "finish ${alg} twitter_xl"
-		echo ""
-#
-#		echo "start ${alg} friendster"
-#		date
-#		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/friendster_dir_b > pr_out/frienster_dir_b_${alg}.txt 2>&1
-#		date
-#		echo "finish ${alg} friendster"
-#		echo ""
-#
-#		echo "start ${alg} LiveJournal"
-#		date
-#		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/LiveJournal_dir_b > pr_out/LiveJournal_dir_b_${alg}.txt 2>&1
-#		date
-#		echo "finish ${alg} LiveJournal"
-#		echo ""
-#
-#		echo "start ${alg} orkut"
-#		date
-#		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/orkut_undir_b > pr_out/orkut_undir_b_${alg}.txt 2>&1
-#		date                 
-#		echo "finish ${alg} orkut"
-#		echo ""
-#
+		# echo "start ${alg} twitter_xl"
+		# date
+		# LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 $BASE_PATH/twitter_undir_xl > pr_out/twitter_undir_xl_${alg}.txt 2>&1
+		# date
+		# echo "finish ${alg} twitter_xl"
+		# echo ""
+
+		# echo "start ${alg} friendster"
+		# date
+		# LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/friendster_dir_b > pr_out/frienster_dir_b_${alg}.txt 2>&1
+		# date
+		# echo "finish ${alg} friendster"
+		# echo ""
+
+		# echo "start ${alg} LiveJournal"
+		# date
+		# LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/LiveJournal_dir_b > pr_out/LiveJournal_dir_b_${alg}.txt 2>&1
+		# date
+		# echo "finish ${alg} LiveJournal"
+		# echo ""
+
+		# echo "start ${alg} orkut"
+		# date
+		# LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 -b /var/shared/projects/asap/graphs/adj/Galois/orkut_undir_b > pr_out/orkut_undir_b_${alg}.txt 2>&1
+		# date                 
+		# echo "finish ${alg} orkut"
+		# echo ""
+
 		echo "start ${alg} USAroad"
 		date
-		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 768 -v edge -rounds 10 /var/shared/projects/asap/graphs/adj/realworld/recent/USAroad_undir > pr_out/USAroad_undir_${alg}.txt 2>&1
+		LD_PRELOAD="./bin/interposer_cilk.so" ./${alg} -c 128 -v edge -rounds 10 $BASE_PATH/USAroad_undir > pr_out/USAroad_undir_${alg}.txt 2>&1
 		date
 		echo "finish ${alg} USAroad"
 		echo ""
